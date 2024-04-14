d5981982d46f059e8069cb0c2dcaa92927dbd030
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-14 14:52:07 +0900

    docs-effective-java-3e

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Effective Java 3/E</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-effective-java-3e',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-effective-java-3e',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
